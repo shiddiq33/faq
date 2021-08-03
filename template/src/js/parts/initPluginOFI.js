a90d41985a14d2b1067809922e6ed84a20369db7
shiddiq33 <banbocor987@gmail.com>
2021-08-03 13:34:11 +0700

    delete folder

--- template/src/js/parts/initPluginOFI.js
+++
@@ -1,9 +0,0 @@
-
-/* Object-Fit-Image */
-function initPluginOFI() {
-    if (typeof window.objectFitImages !== 'undefined') {
-        window.objectFitImages();
-    }
-}
-
-export { initPluginOFI };
