82ecdd8918d4c2e0b5255d2113ac793e2e4c51f2
rizkykamil.dll <rabbaniikamil@gmail.com>
2021-08-12 13:42:18 +0700

    fds

--- ci/application/controllers/LoginController.php
+++ ci/application/controllers/LoginController.php
@@ -1,13 +1,43 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
     public function index($nama = '')
     {
-        $data['judul'] = 'Halaman Home';
-        $data['nama'] = $nama;
-        $this->load->view('templates/header', $data);
-        $this->load->view('home/index', $data);
-        $this->load->view('templates/footer');
+        $this->load->library('form_validation');
+        $this->load->library('session');
+
+        $this->form_validation->set_rules('name', 'Nama', 'required');
+        $this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|max_length[15]|is_unique[users.username]');
+        $this->form_validation->set_rules('password', 'Password', 'required|trim');
+        
+        if ($this->form_validation->run() == FALSE) {
+            $errors = $this->form_validation->error_array();
+            $this->session->set_flashdata('errors', $errors);
+            $this->session->set_flashdata('input', $this->input->post());
+            redirect('auth/register');
+        } else {
+            $username = $this->input->post('username');
+            $name = $this->input->post('name');
+            $password = $this->input->post('password');
+            $pass = password_hash($password, PASSWORD_DEFAULT);
+            $data = [
+                'username' => $username,
+                'name' => $name,
+                'password' => $pass
+            ];
+            $insert = $this->auth_model->register("users", $data);
+            if($insert){
+                echo '<script>alert("Sukses! Anda berhasil melakukan register. Silahkan login untuk mengakses data.");window.location.href="'.base_url('index.php/auth/login').'";</script>';
+            }
+        }
+    }
+
+    public function logout()
+    {
+        $this->session->sess_destroy();
+        echo '<script>alert("Sukses! Anda berhasil logout."); window.location.href="'.base_url('index.php/auth/login').'";
+        </script>';
     }
 }
