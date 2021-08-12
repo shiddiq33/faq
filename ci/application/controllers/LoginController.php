dc6c1a17717a248dc321ebb5c07181b6356fb25a
rizkykamil.dll <rabbaniikamil@gmail.com>
2021-08-12 13:42:50 +0700

    fas

--- ci/application/controllers/LoginController.php
+++ ci/application/controllers/LoginController.php
@@ -1,8 +1,49 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller
-{
+class Auth extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('auth_model');
+        $this->load->library('form_validation');
+        $this->load->library('session');
+    }
+	public function login()
+	{
+		$this->load->view('auth/login');
+    }
+    
+    public function register()
+    {
+        $this->load->view('auth/register');
+    }
+
+    public function proses_login()
+    {
+        $this->load->library('form_validation');
+        $this->load->library('session');
+
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        
+        if ($this->form_validation->run() == FALSE) {
+            $errors = $this->form_validation->error_array();
+            $this->session->set_flashdata('errors', $errors);
+            $this->session->set_flashdata('input', $this->input->post());
+            redirect('/index.php/auth/login'); // LOGIN
+        
+        } else {
+
+            $username = htmlspecialchars($this->input->post('username'));
+            $pass = htmlspecialchars($this->input->post('password'));
+
+            // CEK KE DATABASE BERDASARKAN EMAIL
+            $cek_login = $this->auth_model->cek_login($username); 
+                
+            if($cek_login == FALSE)
+            {
     public function index($nama = '')
     {
         $this->load->library('form_validation');
