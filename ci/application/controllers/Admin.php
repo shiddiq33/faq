0efc5d035da1cfae81016165ed92da58abfd722f
shiddiq33 <banbocor987@gmail.com>
2021-09-07 03:27:27 +0700

    create index and fix mess

--- ci/application/controllers/Admin.php
+++ ci/application/controllers/Admin.php
@@ -6,13 +6,12 @@ class Admin extends CI_Controller
 
     public function index()
     {
-        $data['title'] = 'Dashboard';
-        $data['user'] = $this->db->get_where('users', ['email' =>
-        $this->session->userdata('email')])->row_array();
+        $data['title'] = '';
+        // $data['user'] = $this->db->get_where('users', ['email' =>
+        // $this->session->userdata('email')])->row_array();
 
         $this->load->view('templates/header', $data);
-        // $this->load->view('templates/sidebar', $data);
-        // $this->load->view('templates/topbar', $data);
+        $this->load->view('templates/topbar', $data);
         $this->load->view('admin/index', $data);
         $this->load->view('templates/footer');
     }
